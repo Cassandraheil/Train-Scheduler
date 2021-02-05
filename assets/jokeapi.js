7f926e186a016d74226233a6ebec661067969865
Cassandra Heil <Cassandraheil19@gmail.com>
2021-02-05 16:40:51 -0500

    changed api to free api

--- assets/jokeapi.js
+++ assets/jokeapi.js
@@ -1,12 +1,15 @@
-$.ajax({
-        async: true,
-        crossDomain: true,
-        url: "https://webknox-jokes.p.rapidapi.com/jokes/search?numJokes=5&keywords=kick%2C%20hard&category=train&minRating=5",
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "8a97f2a3f9msh62be3375f02e0ecp1b8eddjsndb0acdbb4d62",
-            "x-rapidapi-host": "webknox-jokes.p.rapidapi.com"
-        }
-    }).then(function (response) {
+console.log("this connected")
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://joke3.p.rapidapi.com/v1/joke?nsfw=false",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "8a97f2a3f9msh62be3375f02e0ecp1b8eddjsndb0acdbb4d62",
+		"x-rapidapi-host": "joke3.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
 	console.log(response);
 });
